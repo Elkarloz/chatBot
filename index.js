c2ab500dc01b82931a1f3a48b54bfa616a5c7e0f
Carlos Mauricio Roa <57810969+Elkarloz@users.noreply.github.com>
2023-07-12 11:55:59 -0500

    Feat ssl with websocket  config

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-const express = require("express"),
-  path = require("path"),
-  route = require("./routes"),
-  app = express(),
-  bodyParser = require("body-parser"),
-  WebSocket = require("ws");
+const express = require("express");
+const https = require("https");
+const fs = require("fs");
+const path = require("path");
+const route = require("./routes");
+const bodyParser = require("body-parser");
+const WebSocket = require("ws");
 
 require("dotenv").config();
+
+const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(__dirname + "/public"));
-app.use(express.json({
-  limit: "10mb"
-}));
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use("/", route.start);
@@ -24,13 +23,16 @@ app.use("/Api/Chat", route.apiChat);
 app.use("/Api/Push", route.apiPush);
 app.use("/Api/Admin", route.apiAdmin);
 
-const server = app.listen(3000, () => {
-  console.log("Servidor corriendo en el puerto 3000...");
-});
+const sslOptions = {
+  key: fs.readFileSync("/etc/letsencrypt/live/desarrollo.tuzumitos.com/privkey.pem"),
+  cert: fs.readFileSync("/etc/letsencrypt/live/desarrollo.tuzumitos.com/fullchain.pem")
+};
 
-const wss = new WebSocket.Server({
-    server
-  }),
-  botController = require("./controllers/BotController");
-
+const server = https.createServer(sslOptions, app);
+const wss = new WebSocket.Server({ server });
+const botController = require("./controllers/BotController");
 botController.main(wss);
+
+server.listen(3000, () => {
+  console.log("Servidor corriendo en el puerto 3000 con SSL...");
+});
